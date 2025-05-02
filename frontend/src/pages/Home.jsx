2fbb7b932d53b4e9aa6c0f5884a46524764ad9d0
Davlatbek <aberdinazarov72@gmail.com>
2025-05-02 16:36:01 +0500

    latest commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -18,7 +18,18 @@ export default function Home() {
   useEffect(() => {
     fetchRequest()
   }, [])
+
+  const handleLogOut = () => {
+    localStoreage.clear()
+    // remove the access token from local storage
+    localStorage.removeItem("accessToken")
+    // remove the refresh token from local storage
+    localStorage.removeItem("refreshToken")
+    window.location.href = "/login"
+  }
   return (
-    <div>Home {request}</div>
+    <div>Home {request}
+      <button className='bg-red-500 p-4 text-white' onClick={handleLogOut}>Log out</button>
+    </div>
   )
 }
